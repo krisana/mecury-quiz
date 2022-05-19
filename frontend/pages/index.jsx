89eb45e80d1d7579404f1f9693b49c2a33668749
Krisana Malaseeta <krisana.malaseeta@gmail.com>
2022-05-19 20:36:01 +0700

    update homepage

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,30 +1,13 @@
-import { useContext, useEffect, useState } from 'react'
-import { AppContext, BaseLayout } from '../layouts'
-import {
-  Table,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap'
-import Select from 'react-select'
-import { getUserData } from '../services'
-import jsHttpCookie from 'cookie';
-import { useSession } from 'next-auth/react';
+import { BaseLayout } from '../layouts'
 
 export default function Home() {
-  const [user, setUser] = useState(null);
-  // const { data: session } = useSession()
-
-  useEffect(() => {
-    getUserData().then((data) => {
-      setUser(data)
-    });
-  }, [])
 
   return (
     <BaseLayout>
-      <h1>Home page</h1>
-      <pre>{`${user}`}</pre>
+      <div className='homepage'>
+        <h1>MEDcury Quiz</h1>
+        <h3>ระบบนัดหมายแพทย์</h3>
+      </div>
     </BaseLayout>
   )
 }
